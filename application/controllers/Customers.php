a5626527a034cfc1f9633742e78ad35c01e1fdda
RhysQuilter <71886055+RhysQuilter@users.noreply.github.com>
2020-12-05 12:12:14 +0000

    Adding simple Cutomer Controller and route

---
+++ application/controllers/Customers.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Customers extends CI_Controller
+{
+
+    public function index()
+    {
+        echo 'Hello Customer World!';
+    }
+}
+        
+    /* End of file  Customer.php */
