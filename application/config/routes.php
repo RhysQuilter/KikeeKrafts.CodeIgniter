7c6d34b420fb8d174edb23331ea141fbc410e8e5
Rhys Quilter <71886055+RhysQuilter@users.noreply.github.com>
2021-01-26 22:12:52 +0000

    config

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,5 +53,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Customers';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['login'] = 'login';
+//$route['login'] = 'login';
 
