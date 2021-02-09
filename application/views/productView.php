3731411ba44e709ba2fbbb533fb4c58af7f7d7cf
Rhys Quilter <71886055+RhysQuilter@users.noreply.github.com>
2021-02-09 21:58:40 +0000

    bootstrap

--- application/views/productView.php
+++ application/views/productView.php
@@ -3,39 +3,53 @@
 
 echo form_open();
 echo '</br></br>';
-
-echo 'Product Code : ';
-echo form_input('Id', $product->Id, 'readonly');
-
 ?>
-<table>
+<table class="table table-striped"
+    <tr>
+        <th scope="row">Product Code:</th>
+        <td><?php echo $product->Id; ?></td>
+    </tr>
+
     <tr>
         <th scope="row">Description:</th>
         <td><?php echo $product->Description; ?></td>
     </tr>
-</table>
-<?Php
 
-echo '</br></br>Category : ';
-echo form_input('Category', $product->Category, 'readonly');
 
-echo '</br></br>Artist : ';
-echo form_input('Artist', $product->Artist, 'readonly');
 
-echo '</br></br>Product in stock : ';
-echo form_input('prodQtyInStock', $product->StockQuantity, 'readonly');
-
-echo '</br></br>Cost : ';
-echo form_input('prodBuyCost', $product->BuyCost, 'readonly');
-
-echo '</br></br>Sale Price : ';
-echo form_input('prodSalePrice', $product->SalePrice, 'readonly');
-
-echo '</br></br>Discount : ';
-echo form_input('priceAlreadyDiscounted', $product->PriceDiscounted, 'readonly');
-
-echo '</br></br>';
-echo '<img src=' . base_url() . "assets/images/products/" . 'full/' . $product->Photo . '>';
-
-echo '</br></br>';
-echo form_close();
+<tr>
+        <th scope="row">Category:</th>
+        <td><?php echo $product->Category; ?></td>
+    </tr>
+    
+    <tr>
+        <th scope="row">Artist:</th>
+        <td><?php echo $product->Artist; ?></td>
+    </tr>
+    
+    <tr>
+        <th scope="row">Product in Stock:</th>
+        <td><?php echo $product->StockQuantity;?></td>
+    </tr>
+    
+     <tr>
+        <th scope="row">Purchase Cost:</th>
+        <td><?php echo $product->BuyCost;?></td>
+    </tr>
+    
+    <tr>
+        <th scope="row">Sale Price:</th>
+        <td><?php echo $product->SalePrice;?></td>
+    </tr>
+    
+    <tr>
+        <th scope="row">Discounted:</th>
+        <td><?php echo $product->PriceDiscounted;?></td>
+    </tr>
+    
+    <br>
+    <tr>
+        <th><?php echo '<img src=' . base_url() . "assets/images/products/" . 'full/' . $product->Photo . '>';?></th>
+        <td>  &nbsp;&nbsp;</td>
+    </tr>
+    </table>
